96fd21d10aa6c56a6b420d3caea76db5aa7ecfdd
Bbosa Muhamood <44940073+bmuhamood@users.noreply.github.com>
2023-10-26 02:38:11 +0400

    final commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["frontend development", "backend development", "Custom Software Development", "Mobile Apps with React Native", "IT Consulting"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
